a339346eea0627b427a8502a89c9aaf4eaba0f1e
timotejblazic <timi.blazic@gmail.com>
2026-03-23 23:23:33 +0100

    testing triangle

--- src/code/run_sample.sh
+++ src/code/run_sample.sh
@@ -3,11 +3,11 @@
 #SBATCH --reservation=fri
 #SBATCH --job-name=code_sample
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=16
 #SBATCH --output=sample_out.log
 #SBATCH --hint=nomultithread
 
-PROGRAM_NAME="parallel_seam"
+PROGRAM_NAME="triangle"
 
 set -e 
 
